0d50cf493fa96fbf819d37db99282a604718dd2b
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2025-01-30 01:08:55 +0900

    update ergogen

--- ergogen/usb-c-6p-f6c16.js
+++ ergogen/usb-c-6p-f6c16.js
@@ -74,11 +74,11 @@ module.exports = {
     const back_paste = `
     `
     const edge_cuts = `
-        (fp_line (start -4.3 3.4) (end -4.3 -2.8) (stroke (width 0.05) (type default)) (layer "Edge.Cuts") )
-        (fp_line (start -3.3 -2.8) (end 3.3 -2.8) (stroke (width 0.05) (type default)) (layer "Edge.Cuts") )
-        (fp_line (start 4.3 -2.8) (end 4.3 3.4) (stroke (width 0.05) (type default)) (layer "Edge.Cuts") )
-        (fp_arc (start -4.3 -2.8) (mid -3.8 -3.3) (end -3.3 -2.8) (stroke (width 0.05) (type default)) (layer "Edge.Cuts") )
-        (fp_arc (start 3.3 -2.8) (mid 3.8 -3.3) (end 4.3 -2.8) (stroke (width 0.05) (type default)) (layer "Edge.Cuts") )
+        (fp_line (start -4.3 3.4) (end -4.3 -2.8) (stroke (width 0.15) (type default)) (layer "Edge.Cuts") )
+        (fp_line (start -3.3 -2.8) (end 3.3 -2.8) (stroke (width 0.15) (type default)) (layer "Edge.Cuts") )
+        (fp_line (start 4.3 -2.8) (end 4.3 3.4) (stroke (width 0.15) (type default)) (layer "Edge.Cuts") )
+        (fp_arc (start -4.3 -2.8) (mid -3.8 -3.3) (end -3.3 -2.8) (stroke (width 0.15) (type default)) (layer "Edge.Cuts") )
+        (fp_arc (start 3.3 -2.8) (mid 3.8 -3.3) (end 4.3 -2.8) (stroke (width 0.15) (type default)) (layer "Edge.Cuts") )
     `
     const user_drawing = `
     `
