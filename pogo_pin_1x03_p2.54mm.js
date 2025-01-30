2fb36c0c642c7f62717107a6904b9cab0ea1c9a8
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2025-01-30 16:32:48 +0900

    updated ergogen
    * added pogo pins/tags

---
+++ pogo_pin_1x03_p2.54mm.js
@@ -0,0 +1,93 @@
+module.exports = {
+  params: {
+    designator: 'J',    // change it accordingly
+    side: 'F',          // delete if not needed
+    P3: {type: 'net', value: 'P3'}, // undefined}, // change to undefined as needed
+    P2: {type: 'net', value: 'P2'}, // undefined}, // change to undefined as needed
+    P1: {type: 'net', value: 'P1'}, // undefined}, // change to undefined as needed
+  },
+  body: p => {
+    const standard_opening = `(
+         footprint "TB2086_MISC:Pogopin_1x03_P2.54mm"
+        (version 20240108)
+        (generator "pcbnew")
+        (generator_version "8.0")
+        (layer "F.Cu")
+        (property "Reference" "${p.ref}" (at 0 -1.77 ${0 + p.rot}) (unlocked yes) (layer "${p.side}.SilkS") (hide yes)  (effects (font (size 1 1) (thickness 0.1))))
+        (attr smd)
+        ${p.at /* parametric position */}
+    `
+    const front_silkscreen = `
+    `
+    const front_pads = `
+    `
+    const front_fabrication = `
+        (property "Value" "Pogopin_1x03_P2.54mm" (at 0 2 ${0 + p.rot}) (unlocked yes) (layer "F.Fab")  (effects (font (size 1 1) (thickness 0.15))))
+        (property "Footprint" "" (at 0 0 ${0 + p.rot}) (unlocked yes) (layer "F.Fab") (hide yes)  (effects (font (size 1 1) (thickness 0.15))))
+        (property "Datasheet" "" (at 0 0 ${0 + p.rot}) (unlocked yes) (layer "F.Fab") (hide yes)  (effects (font (size 1 1) (thickness 0.15))))
+        (property "Description" "" (at 0 0 ${0 + p.rot}) (unlocked yes) (layer "F.Fab") (hide yes)  (effects (font (size 1 1) (thickness 0.15))))
+        (fp_text user "\${REFERENCE}" (at 0 3.5 ${0 + p.rot}) (unlocked yes) (layer "F.Fab")  (effects (font (size 1 1) (thickness 0.15))))
+    `
+    const front_mask = `
+    `
+    const front_courtyard = `
+    `
+    const front_paste = `
+    `
+    const pads = `
+        (pad "1" thru_hole circle (at -2.54 0 ${p.rot}) (size 2.1 2.1) (drill 0.9) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P1})
+        (pad "2" thru_hole circle (at 0 0 ${p.rot}) (size 2.1 2.1) (drill 0.9) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P2})
+        (pad "3" thru_hole circle (at 2.54 0 ${p.rot}) (size 2.1 2.1) (drill 0.9) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.P3})
+    `
+    const back_silkscreen = `
+    `
+    const back_pads = `
+    `
+    const back_fabrication = `
+    `
+    const back_mask = `
+    `
+    const back_courtyard = `
+    `
+    const back_paste = `
+    `
+    const edge_cuts = `
+    `
+    const user_drawing = `
+    `
+    const user_comments = `
+    `
+    const user_eco1 = `
+    `
+    const user_eco2 = `
+    `
+    const model = `
+    `
+    const standard_closing = `
+            )
+    `
+    let final = standard_opening;
+    final += front_silkscreen;
+    final += front_pads;
+    final += front_fabrication;
+    final += front_mask;
+    final += front_courtyard;
+    final += front_paste;
+    final += pads;
+    final += back_silkscreen;
+    final += back_pads;
+    final += back_fabrication;
+    final += back_mask;
+    final += back_courtyard;
+    final += back_paste;
+    final += edge_cuts;
+    final += user_drawing;
+    final += user_comments;
+    final += user_eco1;
+    final += user_eco2;
+    final += model;
+    final += standard_closing;
+
+    return final
+  }
+}
